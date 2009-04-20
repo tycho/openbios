c05e75882c49a75a713e81d3c43bfcba8d260eca
Steven Noonan <steven@uplinklabs.net>
2009-04-19 20:47:06 -0700

    qemu/main.c: load embedded XCOFF binaries in CHRP scripts
    Mac OS X does this with BootX files. It's important to note
    that the way in which this is currently loaded is a hack
    until the Forth interpreter properly handles the Mac OS X
    CHRP scripts.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- arch/ppc/qemu/main.c
+++ arch/ppc/qemu/main.c
@@ -36,7 +36,203 @@
 #endif
 #define CHRP_DPRINTF(fmt, args...) SUBSYS_DPRINTF("CHRP", fmt, ##args)
 #define ELF_DPRINTF(fmt, args...) SUBSYS_DPRINTF("ELF", fmt, ##args)
+#define XCOFF_DPRINTF(fmt, args...) SUBSYS_DPRINTF("XCOFF", fmt, ##args)
 #define YABOOT_DPRINTF(fmt, args...) SUBSYS_DPRINTF("YABOOT", fmt, ##args)
+
+
+/* XCOFF executable loader */
+typedef struct COFF_filehdr_t {
+    uint16_t f_magic;	/* magic number			*/
+    uint16_t f_nscns;	/* number of sections		*/
+    uint32_t f_timdat;	/* time & date stamp		*/
+    uint32_t f_symptr;	/* file pointer to symtab	*/
+    uint32_t f_nsyms;	/* number of symtab entries	*/
+    uint16_t f_opthdr;	/* sizeof(optional hdr)		*/
+    uint16_t f_flags;	/* flags			*/
+} COFF_filehdr_t;
+
+/* IBM RS/6000 */
+#define U802WRMAGIC     0x02DA    /* writeable text segments **chh**      */
+#define U802ROMAGIC     0x02DF    /* readonly sharable text segments      */
+#define U802TOCMAGIC    0x02E1    /* readonly text segments and TOC       */
+#define U802TOMAGIC     0x01DF
+
+/*
+ *   Bits for f_flags:
+ *
+ *	F_RELFLG	relocation info stripped from file
+ *	F_EXEC		file is executable  (i.e. no unresolved external
+ *			references)
+ *	F_LNNO		line numbers stripped from file
+ *	F_LSYMS		local symbols stripped from file
+ *	F_MINMAL	this is a minimal object file (".m") output of fextract
+ *	F_UPDATE	this is a fully bound update file, output of ogen
+ *	F_SWABD		this file has had its bytes swabbed (in names)
+ *	F_AR16WR	this file has the byte ordering of an AR16WR
+ *			(e.g. 11/70) machine
+ *	F_AR32WR	this file has the byte ordering of an AR32WR machine
+ *			(e.g. vax and iNTEL 386)
+ *	F_AR32W		this file has the byte ordering of an AR32W machine
+ *			(e.g. 3b,maxi)
+ *	F_PATCH		file contains "patch" list in optional header
+ *	F_NODF		(minimal file only) no decision functions for
+ *			replaced functions
+ */
+
+#define  COFF_F_RELFLG		0000001
+#define  COFF_F_EXEC		0000002
+#define  COFF_F_LNNO		0000004
+#define  COFF_F_LSYMS		0000010
+#define  COFF_F_MINMAL		0000020
+#define  COFF_F_UPDATE		0000040
+#define  COFF_F_SWABD		0000100
+#define  COFF_F_AR16WR		0000200
+#define  COFF_F_AR32WR		0000400
+#define  COFF_F_AR32W		0001000
+#define  COFF_F_PATCH		0002000
+#define  COFF_F_NODF		0002000
+
+typedef struct COFF_aouthdr_t {
+    uint16_t magic;      /* type of file			  */
+    uint16_t vstamp;     /* version stamp		          */
+    uint32_t tsize;      /* text size in bytes, padded to FW bdry */
+    uint32_t dsize;      /* initialized data "  "	          */
+    uint32_t bsize;      /* uninitialized data "   "	          */
+    uint32_t entry;	 /* entry pt.			          */
+    uint32_t text_start; /* base of text used for this file       */
+    uint32_t data_start; /* base of data used for this file       */
+    uint32_t o_toc;	 /* address of TOC                        */
+    uint16_t o_snentry;	 /* section number of entry point         */
+    uint16_t o_sntext;	 /* section number of .text section       */
+    uint16_t o_sndata;	 /* section number of .data section       */
+    uint16_t o_sntoc;	 /* section number of TOC                 */
+    uint16_t o_snloader; /* section number of .loader section     */
+    uint16_t o_snbss;	 /* section number of .bss section        */
+    uint16_t o_algntext; /* .text alignment                       */
+    uint16_t o_algndata; /* .data alignment                       */
+    uint16_t o_modtype;	 /* module type (??)                      */
+    uint16_t o_cputype;	 /* cpu type                              */
+    uint32_t o_maxstack; /* max stack size (??)                   */
+    uint32_t o_maxdata;	 /* max data size (??)                    */
+    char o_resv2[12];	 /* reserved                              */
+} COFF_aouthdr_t;
+
+#define AOUT_MAGIC	0x010b
+
+typedef struct COFF_scnhdr_t {
+    char s_name[8];	/* section name			    */
+    uint32_t s_paddr;	/* physical address, aliased s_nlib */
+    uint32_t s_vaddr;	/* virtual address		    */
+    uint32_t s_size;	/* section size			    */
+    uint32_t s_scnptr;	/* file ptr to raw data for section */
+    uint32_t s_relptr;	/* file ptr to relocation	    */
+    uint32_t s_lnnoptr;	/* file ptr to line numbers	    */
+    uint16_t s_nreloc;	/* number of relocation entries	    */
+    uint16_t s_nlnno;	/* number of line number entries    */
+    uint32_t s_flags;	/* flags			    */
+} COFF_scnhdr_t;
+
+
+static int
+load_xcoff(int fd, char **dest, char **entrypt, char **end)
+{
+    COFF_filehdr_t fhdr;
+    COFF_aouthdr_t ahdr;
+    COFF_scnhdr_t shdr;
+    char *first, *last;
+    uint64_t loffset, ept_offset;
+    uint32_t offset, actual_entry;
+    int i;
+
+    XCOFF_DPRINTF("Loading '%s'\n", get_file_path(fd));
+
+    loffset = tell(fd);
+
+    if (read_io(fd, &fhdr, sizeof(COFF_filehdr_t)) < 0) {
+        XCOFF_DPRINTF("Cannot load first bloc of file...\n");
+        return -1;
+    }
+    if (fhdr.f_magic != U802WRMAGIC && fhdr.f_magic != U802ROMAGIC &&
+        fhdr.f_magic != U802TOCMAGIC && fhdr.f_magic != U802TOMAGIC) {
+        XCOFF_DPRINTF("Not a XCOFF file %02x\n", fhdr.f_magic);
+        return -2;
+    }
+    if (fhdr.f_magic != 0x01DF && (fhdr.f_flags & COFF_F_EXEC) == 0) {
+        XCOFF_DPRINTF("Not an executable XCOFF file %02x\n", fhdr.f_flags);
+        return -2;
+    }
+    if (fhdr.f_opthdr != sizeof(COFF_aouthdr_t)) {
+        XCOFF_DPRINTF("AOUT optional error size missmactch in XCOFF file\n");
+        return -2;
+    }
+    if (read_io(fd, &ahdr, sizeof(COFF_aouthdr_t)) < 0) {
+        XCOFF_DPRINTF("Cannot load XCOFF AOUT header...\n");
+        return -1;
+    }
+    if (ahdr.magic != AOUT_MAGIC) {
+        XCOFF_DPRINTF("Invalid AOUT optional header\n");
+        return -2;
+    }
+
+    *entrypt = (char *)ahdr.entry;
+    last = NULL;
+    first = last - 4;
+    offset = sizeof(COFF_filehdr_t) + sizeof(COFF_aouthdr_t);
+    XCOFF_DPRINTF("XCOFF file with %d sections\n", fhdr.f_nscns);
+    for (i = 0; i < fhdr.f_nscns; i++) {
+        XCOFF_DPRINTF("Read next header (%0x)\n", offset);
+        seek_io(fd, offset + loffset);
+        if (read_io(fd, &shdr, sizeof(COFF_scnhdr_t)) < 0) {
+            XCOFF_DPRINTF("Cannot load section header %d...\n", i);
+            return -1;
+        }
+	if (strcmp(shdr.s_name, ".text") == 0 ||
+            strcmp(shdr.s_name, ".data") == 0) {
+            if ((char *)shdr.s_vaddr < first)
+                first = (char *)shdr.s_vaddr;
+            if ((char *)shdr.s_vaddr > last)
+                last = (char *)shdr.s_vaddr;
+            XCOFF_DPRINTF("Load '%s' section from %0x %0x to %0x (%0x)\n",
+                    shdr.s_name, offset, shdr.s_scnptr,
+                    shdr.s_vaddr, shdr.s_size);
+
+            if (((ulong)(*entrypt) >= shdr.s_vaddr) &&
+                ((ulong)(*entrypt) < shdr.s_vaddr + shdr.s_size)) {
+                ept_offset = (ulong)(*entrypt) - shdr.s_vaddr + shdr.s_scnptr;
+                XCOFF_DPRINTF("Found entry point offset in '%s': %llx\n",
+                    shdr.s_name, ept_offset);
+            }
+
+            seek_io(fd, shdr.s_scnptr + loffset);
+
+            if (read_io(fd, (void *)shdr.s_vaddr, shdr.s_size) < 0) {
+                XCOFF_DPRINTF("Cannot load section %d...\n", i);
+                return -1;
+            }
+        } else if (strcmp(shdr.s_name, ".bss") == 0) {
+            if ((char *)shdr.s_vaddr < first)
+                first = (char *)shdr.s_vaddr;
+            if ((char *)shdr.s_vaddr > last)
+                last = (char *)shdr.s_vaddr;
+            XCOFF_DPRINTF("Erase '%s' section at %0x size: %0x\n",
+                    shdr.s_name, shdr.s_vaddr, shdr.s_size);
+            memset((void *)shdr.s_vaddr, 0, shdr.s_size);
+        } else {
+            XCOFF_DPRINTF("Skip '%s' section\n", shdr.s_name);
+        }
+        offset += sizeof(COFF_scnhdr_t);
+    }
+    *dest = first;
+    *end = last;
+
+    seek_io(fd, loffset + ept_offset);
+    read_io(fd, &actual_entry, 4);
+    *entrypt = (char*)actual_entry;
+    XCOFF_DPRINTF("Found actual entry point: %08x\n",
+        actual_entry);
+
+    return 0;
+}
 
 static void
 transfer_control_to_elf( ulong elf_entry )
@@ -232,6 +428,7 @@ try_chrp_script(const char *of_path, const char *param, const char *script_path)
     int fd, len, tag, taglen, script, scriptlen, entity;
     char tagbuf[256], bootscript[256], c;
     char *device, *filename, *directory;
+    void *dest, *entrypt, *end;
     int partition;
 
     device = get_device(of_path);
@@ -318,7 +515,12 @@ try_chrp_script(const char *of_path, const char *param, const char *script_path)
 
     feval(bootscript);
 
-    /* check for an embedded XCOFF binary */
+    /*
+        check for an embedded XCOFF binary
+
+        XXX: This is a hack which can be removed when the Forth
+        interpreter properly handles CHRP scripts used by Mac OS X.
+     */
 
     /* eat newline */
     if (read_io(fd, tagbuf, 1) < 0)
@@ -329,8 +531,11 @@ try_chrp_script(const char *of_path, const char *param, const char *script_path)
         goto badf;
 
     /* next bytes should be XCOFF magic */
+    dest = entrypt = end = NULL;
+    if(load_xcoff(fd, (void*)&dest, (void*)&entrypt, (void*)&end) < 0)
+        goto badf;
 
-    /* TODO: Add XCOFF loader here. */
+    transfer_control_to_elf((ulong)entrypt);
 
  badf:
     close_io( fd );
